4b20957793a5c778a6cc854ba4f81bd124224ce7
sergey vetyutnev <serg.vetyutnev@gmail.com>
2017-02-03 13:51:56 +0100

    Fix for #124: Integrate the Restcomm Statistics

--- core/domain/src/main/java/org/mobicents/smsc/server/bootstrap/Version.java
+++ core/domain/src/main/java/org/mobicents/smsc/server/bootstrap/Version.java
@@ -87,7 +87,7 @@ public final class Version {
     }
 
     public String getProjectType() {
-        String version = instance.getProperty("version");
+        String version = instance.getProperty("project.type");
         if (version != null) {
             return version;
         } else {
@@ -96,7 +96,7 @@ public final class Version {
     }
 
     public String getProjectVersion() {
-        String version = instance.getProperty("project.type");
+        String version = instance.getProperty("version");
         if (version != null) {
             return version;
         } else {
