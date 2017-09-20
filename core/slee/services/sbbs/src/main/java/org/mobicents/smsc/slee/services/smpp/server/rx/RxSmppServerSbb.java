13afbd9af398be00388aee15adccd95a9b951c18
Olena Radiboh <olena.radiboh@telestax.com>
2017-09-20 11:34:10 +0300

    Corrected log message for onDeliveryError to not log whole exception (ticket 34713)

--- core/slee/services/sbbs/src/main/java/org/mobicents/smsc/slee/services/smpp/server/rx/RxSmppServerSbb.java
+++ core/slee/services/sbbs/src/main/java/org/mobicents/smsc/slee/services/smpp/server/rx/RxSmppServerSbb.java
@@ -258,7 +258,7 @@ public abstract class RxSmppServerSbb extends DeliveryCommonSbb implements Sbb {
                 else if (event.getResponse() != null)
                     seqNumber = event.getResponse().getSequenceNumber();
 
-                this.onDeliveryError(smsSet, ErrorAction.temporaryFailure, ErrorCode.SC_SYSTEM_ERROR, "SendPduStatus: " + event,
+                this.onDeliveryError(smsSet, ErrorAction.temporaryFailure, ErrorCode.SC_SYSTEM_ERROR, "SendPduStatus: " + event.getException().getMessage(),
                         EventType.OUT_SMPP_ERROR, seqNumber);
             } catch (Throwable e1) {
                 logger.severe("Exception in RxSmppServerSbb.onSendPduStatus(): " + e1.getMessage(), e1);
